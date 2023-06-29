f3349a2597a1e3a01fd5a16a22f3ad35430e1c32
Web Ghost <malsadefs@gmail.com>
2023-06-29 10:12:22 +0300

    Login Guest Added

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,18 +1,20 @@
-import { useTitle } from "../../hooks/useTitle";
-import { Hero } from "./components/Hero";
-import { FeaturedProducts } from "./components/FeaturedProducts";
-import { Testimonials } from "./components/Testimonials";
-import { Faq } from "./components/Faq";
+import { Hero } from "./components/Hero"
+import { FeaturedProducts } from "./components/FeaturedProducts"
+import { Testimonials } from "./components/Testimonials"
+import { Faq } from "./components/Faq"
+import { useTitle } from "../../hooks/useTitle"
+
 
 export const HomePage = () => {
-  useTitle("Access Latest Computer Science E-Books");
 
+  useTitle("Access Latest Computer Science E-Books")
+  
   return (
-    <main>
+    <main >
       <Hero />
       <FeaturedProducts />
       <Testimonials />
       <Faq />
     </main>
-  );
-};
+  )
+}
