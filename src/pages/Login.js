0f5f5a9b470fc7f633b0f57200702a34ace9a1c9
Web Ghost <malsadefs@gmail.com>
2023-06-29 10:42:09 +0300

    Refresh the page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
+import { login } from "../services";
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { login } from "../services";
 import { useTitle } from "../hooks/useTitle";
 
 export const Login = () => {
